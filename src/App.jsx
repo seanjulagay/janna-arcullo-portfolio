b3335a8ba37570e3122b827751950b9123991556
Sean <63425198+seanjulagay@users.noreply.github.com>
2024-01-18 22:19:22 +0800

    Fixed case sensitivity issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Uniqto from "./components/projects/Uniqto.jsx";
+import Uniqto from "./components/Projects/Uniqto.jsx";
 
 export default function App() {
   return (
